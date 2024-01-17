902d7f01081e2c3fe0ed4bd259de221f29862c65
ATHARV DALAL <sangamdalal24@gmail.com>
2024-01-17 12:18:26 +0530

    Middleware adding

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import connectDB from "./db/index.js";
 
+connectDB().then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+       console.log(`Server is running On PORT${process.env.PORT}`); 
+    })
+}).catch((err)=>{
+    console.log("MONGO db Connection Failed !");
+});
 
 
 
-connectDB()
+
+
+
 
 
 
