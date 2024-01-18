ebe347b528fc04464683cd56b93b902bb8896420
ATHARV DALAL <sangamdalal24@gmail.com>
2024-01-18 10:57:48 +0530

    Created VideoModel and use mongooseAggregatePaginate,

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, {Schema} from 'mongoose'
+import jwt from 'jsonwebtoken'
+import bcrypt from "bcrypt"
 
 const userSchema = new Schema({
   username: {
@@ -46,5 +48,50 @@ const userSchema = new Schema({
     type: String,
   }
 }, {timestamps: true})
+
+
+
+userSchema.pre("save", async function (next){
+  if(!this.isModified("password")) return next();
+
+  this.passowrd = bcrypt.hash(this.passowrd, 10)
+  next();
+})
+
+userSchema.methods.isPasswordCorrect = async function (passowrd){
+return await bcrypt.compare(passowrd, this.passowrd , true)
+}
+
+
+userSchema.methods.generateAccessToken = function(){
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname
+    },
+    process.env.ACCES_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCES_TOKEN_EXPIRY
+    }
+  )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    }
+  )
+}
+
 
 export const User = mongoose.model("User", userSchema)
