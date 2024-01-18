95d85a1279370b6216523ec0bc926b116b3a18fb
ATHARV DALAL <sangamdalal24@gmail.com>
2024-01-18 22:33:51 +0530

    Register User Feature

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 connectDB().then(()=>{
-    app.listen(process.env.PORT || 8000,()=>{
-       console.log(`Server is running On PORT${process.env.PORT}`); 
+    app.listen(process.env.PORT || 8001,()=>{
+       console.log(`Server is running On PORT ${process.env.PORT || 8001}`); 
     })
 }).catch((err)=>{
     console.log("MONGO db Connection Failed !");
