880e852a20c94751c3f1a0c0f94007e88c56a62c
Atharv Dalal <sangamdalal24@gmail.com>
2025-01-22 12:32:33 +0530

    error

--- src/index.js
+++ src/index.js
@@ -1,32 +1,15 @@
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
-
-connectDB().then(()=>{
-    app.listen(process.env.PORT || 8001,()=>{
-       console.log(`Server is running On PORT ${process.env.PORT || 8001}`); 
-    })
-}).catch((err)=>{
+let a = 10; // ci demo
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8001, () => {
+      console.log(`Server is running On PORT ${process.env.PORT || 8001}`);
+    });
+  })
+  .catch((err) => {
     console.log("MONGO db Connection Failed !");
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  });
 
 // import express  from "express";
 // const app = express();
